6bbe8250dcb3f289b7e75a3faad6fbee960911ed
juliosilva111 <silvajulioc22@gmail.com>
2026-09-16 17:50:44 -0300

    corrige inicializacao do banco nos testes

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import helmet from "helmet";
 import rateLimit from "express-rate-limit";
-import db from "./database.js";
+import { createDatabase } from "./database.js";
 import { usuarios } from "./schema.js";
 
 const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -28,7 +28,7 @@ function validarUsuario(body) {
     return null;
 }
 
-export function createApp(database = db) {
+export function createApp(database = createDatabase()) {
     const app = express();
 
     app.disable("x-powered-by");
